844fa3cf3b77ce5e0767e0b51b00fa1f8f7ae406
Faubette <yannfaubet@yahoo.fr>
2021-10-10 12:16:37 +0200

    [Yathzee | FAUBET] : Implementation de la structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Yathzee
+
+ddd
---
+++ src/Yahtzee/YahtzeeMainTest.java
@@ -0,0 +1,14 @@
+package Yahtzee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class YahtzeeMainTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
